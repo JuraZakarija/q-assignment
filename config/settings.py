09514a733dd29cb70f75dc775c97892a4b4dfbb6
JuraZakarija <zakarijaj@gmail.com>
2022-06-17 02:37:09 +0200

    add jwt auth

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
